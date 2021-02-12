ed5cf3a4be7b0a80664f04f1c675158f3f346a46
Ben Rutland <benrutland@hotmail.com>
2021-02-12 17:52:45 +0000

    added file comments

--- routes/ajax.php
+++ routes/ajax.php
@@ -2,16 +2,16 @@
 
 use Radiate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Ajax Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register ajax routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "ajax" middleware group. Now create something great!
-|
-*/
+/**
+ * -----------------------------------------------------------------------------
+ *  Ajax Routes
+ * -----------------------------------------------------------------------------
+ *
+ *  Here is where you can register ajax routes for your application. These
+ *  routes are loaded by the RouteServiceProvider within a group which
+ *  contains the "ajax" middleware group. Now create something great!
+ *
+ */
 
 Route::ajax('welcome', function () {
     return 'welcome';
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,16 @@
 
 use Radiate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/**
+ *-----------------------------------------------------------------------------
+ * API Routes
+ *-----------------------------------------------------------------------------
+ *
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * is assigned the "api" middleware group. Enjoy building your API!
+ *
+ */
 
 Route::get('welcome', function () {
     return 'welcome';
