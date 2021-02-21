e9de3c35e346ff7ae0372f440b4dfa0523874d26
Ben Rutland <benrutland@hotmail.com>
2021-02-21 15:58:52 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -85,9 +85,9 @@ $app->register(Radiate\Validation\ValidationServiceProvider::class);
 $app->register(Radiate\View\ViewServiceProvider::class);
 $app->register(Radiate\WordPress\WordPressServiceProvider::class);
 
-$app->register(Theme\Providers\EventServiceProvider::class);
-$app->register(Theme\Providers\RouteServiceProvider::class);
-$app->register(Theme\Providers\WordPressServiceProvider::class);
+$app->register(Plugin\Providers\EventServiceProvider::class);
+$app->register(Plugin\Providers\RouteServiceProvider::class);
+$app->register(Plugin\Providers\WordPressServiceProvider::class);
 
 /**
  * -----------------------------------------------------------------------------
