733e2e36dcc1bbbd7cbb9257b97980b8d1f7b73c
Ben Rutland <benrutland@hotmail.com>
2021-06-17 21:19:58 +0100

    updated auth config

--- config/auth.php
+++ config/auth.php
@@ -4,17 +4,42 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Provider Default
+    | Authentication Defaults
     |--------------------------------------------------------------------------
     |
-    | This option controls the default authentication "provider". You may
-    | change this default as required.
-    |
-    | Supported: "radiate", "wordpress"
+    | This option controls the default authentication "guard" and password
+    | reset options for your application. You may change these defaults
+    | as required, but they're a perfect start for most applications.
     |
     */
 
-    'default' => 'radiate',
+    'defaults' => [
+        'guard' => 'web',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Guards
+    |--------------------------------------------------------------------------
+    |
+    | Next, you may define every authentication guard for your application.
+    | Of course, a great default configuration has been defined for you
+    | here which uses session storage and the Eloquent user provider.
+    |
+    | All authentication drivers have a user provider. This defines how the
+    | users are actually retrieved out of your database or other storage
+    | mechanisms used by this application to persist your user's data.
+    |
+    | Supported: "session"
+    |
+    */
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -22,19 +47,26 @@ return [
     |--------------------------------------------------------------------------
     |
     | All authentication drivers have a user provider. This defines how the
-    | users are accessed from the authentication system.
+    | users are actually retrieved out of your database or other storage
+    | mechanisms used by this application to persist your user's data.
+    |
+    | If you have multiple user tables or models you may configure multiple
+    | sources which represent each model / table. These sources may then
+    | be assigned to any extra authentication guards you have defined.
+    |
+    | Supported: "wordpress", "radiate"
     |
     */
 
     'providers' => [
-
-        'radiate' => [
-            'driver' => 'radiate',
-            'model'  => \Plugin\Models\User::class,
+        'users' => [
+            'driver' => 'wordpress',
+            'table' => 'users',
         ],
 
-        'wordpress' => [
-            'driver' => 'wordpress',
+        'users' => [
+            'driver' => 'radiate',
+            'model' => \Plugin\Models\User::class,
         ],
     ],
 ];
